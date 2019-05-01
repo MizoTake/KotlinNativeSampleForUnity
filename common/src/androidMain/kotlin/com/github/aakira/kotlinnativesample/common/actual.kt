0dccf52be944dfae1eef8a357d331a2bcb484887
MizoTake <mizotake123@gmail.com>
2019-05-01 23:09:15 +0900

    remote modules

--- common/src/androidMain/kotlin/com/github/aakira/kotlinnativesample/common/actual.kt
+++
@@ -1,5 +0,0 @@
-package com.github.aakira.kotlinnativesample.common
-
-actual fun platformName(): String {
-    return "Android"
-}
--- common/src/iosMain/kotlin/com/github/aakira/kotlinnativesample/common/actual.kt
+++
@@ -1,9 +0,0 @@
-package com.github.aakira.kotlinnativesample.common
-
-import platform.UIKit.UIDevice
-
-actual fun platformName(): String {
-    return UIDevice.currentDevice.systemName() +
-            " " +
-            UIDevice.currentDevice.systemVersion
-}
